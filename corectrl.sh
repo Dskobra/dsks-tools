cdc65441e4604b61b1015c55df34242068eda788
Jordan Bottoms <jbttoms@gmail.com>
2024-12-13 03:39:38 -0600

    Added menu

---
+++ corectrl.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+## setup corectrl by running the corectrl.py script
+python3 corectrl.py
+sudo cp "90-corectrl.rules" "/etc/polkit-1/rules.d/90-corectrl.rules"
+sudo chown root:root "/etc/polkit-1/rules.d/90-corectrl.rules"
---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+export $TOOLS_FOLDER                          # stores full path for dsks-tools
+export COPYRIGHT="Copyright (c) 2024 Jordan Bottoms"
+export VERSION=""
+TOOLS_FOLDER=$(pwd)
+"$TOOLS_FOLDER"/menu.sh
