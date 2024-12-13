bb2abf482ce6a9641959bfd4c3b143851b2ebb0d
Jordan Bottoms <jbttoms@gmail.com>
2024-12-13 03:45:34 -0600

    Mark as executable

--- corectrl.sh
+++ corectrl.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 ## setup corectrl by running the corectrl.py script
+cd "configs" || exit
 python3 corectrl.py
 sudo cp "90-corectrl.rules" "/etc/polkit-1/rules.d/90-corectrl.rules"
 sudo chown root:root "/etc/polkit-1/rules.d/90-corectrl.rules"
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-export $TOOLS_FOLDER                          # stores full path for dsks-tools
+export TOOLS_FOLDER                          # stores full path for dsks-tools
 export COPYRIGHT="Copyright (c) 2024 Jordan Bottoms"
 export VERSION=""
 TOOLS_FOLDER=$(pwd)
