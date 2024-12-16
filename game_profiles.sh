f96725e6dfe022a42f76f9e79900ec957e81260a
Jordan Bottoms <jbttoms@gmail.com>
2024-12-15 22:30:32 -0600

    Update game_profiles.sh
    Make sure mangohud folder is created

--- game_profiles.sh
+++ game_profiles.sh
@@ -5,6 +5,7 @@
 if test -f /home/"$USER"/.config/MangoHud/MangoHud.conf; then
     echo "MangoHud.conf exists. Not copying profiles over."
 elif ! test -f /home/"$USER"/.config/MangoHud/MangoHud.conf; then
+    mkdir /home/"$USER"/.config/MangoHud/
     git clone https://github.com/Dskobra/game-profiles
     cd game-profiles || exit
     chown "$USER":"$USER" *.conf
