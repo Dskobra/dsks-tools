3932bba2eeb7fda39954ef8f63cbab33faca1ed8
Jordan Bottoms <jbttoms@gmail.com>
2025-01-01 23:12:14 -0600

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -54,7 +54,7 @@ security(){
     # https://discussion.fedoraproject.org/t/use-clamdscan-on-workstation-f38/96864/3
     # note not sure why setroubleshoot package isnt preinstalled in fedora 41 kde spin.
     # fedora 40 was preinstalled
-    sudo dnf install -y clamav clamav-update clamd setroubleshoot
+    sudo dnf install -y clamav clamav-update clamd #setroubleshoot
     sudo sed -i -e "/^#*LocalSocket\s/s/^#//" /etc/clamd.d/scan.conf
     sudo freshclam
     sudo systemctl --now enable clamav-freshclam.service clamd@scan.service
@@ -76,7 +76,7 @@ then
     game_profiles
 elif [ "$1" == "security" ]
 then
-    clamav
+    security
 else
     echo "error"
 fi
