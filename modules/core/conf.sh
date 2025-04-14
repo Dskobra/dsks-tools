0879b1015c3872131aec6eae07d75f7dbba4a781
Dskobra <jbttoms@gmail.com>
2025-04-14 01:07:28 -0500

    fixed incorrect path

--- modules/core/conf.sh
+++ modules/core/conf.sh
@@ -101,7 +101,7 @@ setup_zram(){
     fi
 
     sudo cp /usr/lib/systemd/zram-generator.conf zram-generator.conf.bak
-    sudo cp "$TOOLS_FOLDER"/configs/zram-generator.conf /usr/lib/systemd/zram-generator.conf
+    sudo cp "$TOOLS_FOLDER"/modules/configs/zram-generator.conf /usr/lib/systemd/zram-generator.conf
     sudo chown root:root /usr/lib/systemd/zram-generator.conf
     sudo zramswapon
 }
