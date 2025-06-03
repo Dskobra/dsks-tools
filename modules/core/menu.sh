c7295f5c0ffc88c6284d121649501e53fa90c34e
Dskobra <jbttoms@gmail.com>
2025-06-02 23:30:06 -0500

    reorganizing
    added new script to configure my desktop

--- modules/core/menu.sh
+++ modules/core/menu.sh
@@ -11,8 +11,8 @@ main_menu(){
     echo ""
     echo "(1) Setup Grub                     (2) Setup Corectrl"
     echo "(3) Game Profiles                  (4) Setup zram"
-    echo "(5) Nvidia gsk fix                 (6) Steam launch fix"
-    echo "(7) Setup clamd                    (8) Remove rpmfusion"
+    echo "(5) Various fixes                  (6) Setup clamd"
+    echo "(7) Remove rpmfusion               (8) Devices"
     echo "(0) Exit"
     printf "Option: "
     read -r input
@@ -37,19 +37,19 @@ main_menu(){
             ;;
 
         5)
-            "$TOOLS_FOLDER"/modules/fixes/nvidia.sh
+            fixes_menu
             ;;
 
         6)
-            "$TOOLS_FOLDER"/modules/fixes/steam.sh
-            ;;
-
-        7)
             "$TOOLS_FOLDER"/modules/fixes/clamd.sh
             ;;
 
-        8)
+        7)
             "$TOOLS_FOLDER"/modules/fixes/rpmfusion.sh
+            ;;
+
+        8)
+            post_menu
             ;;
 
         0)
@@ -66,6 +66,7 @@ main_menu(){
         unset input
         main_menu
 }
+
 grub_menu(){
     echo "(1) AMD                           (2) NVIDIA"    
     echo "(m) Main Menu                     (0) Exit"    
@@ -105,4 +106,87 @@ grub_menu(){
         unset input
 }
 
+post_menu(){
+    echo "              Device menu"
+    echo "(1) Desktop                       (2) Laptop"
+    echo "(m) Main Menu                     (0) Exit"
+    printf "Option: "
+    read -r input
+
+    case $input in
+
+
+        1)
+            "$TOOLS_FOLDER"/modules/post_install/desktop.sh
+            ;;
+
+        2)
+            echo "Not finished"
+            ;;
+
+        m)
+            main_menu
+            ;;
+
+        M)
+            main_menu
+            ;;
+
+        0)
+            exit
+            ;;
+
+        *)
+            echo -n "Unknown entry"
+            echo ""
+            post_menu
+            ;;
+
+        esac
+        unset input
+}
+
+fixes_menu(){
+    echo "------------------"
+    echo "|   DSK's Tools  |"
+    echo "------------------"
+    echo ""
+    echo "$COPYRIGHT"
+    echo "Released under the MIT license"
+    echo ""
+    echo ""
+    echo "(1) Nvidia gsk fix                 (2) Steam launch fix"
+    echo "(0) Exit"
+    printf "Option: "
+    read -r input
+
+    case $input in
+
+
+        1)
+            "$TOOLS_FOLDER"/modules/fixes/nvidia.sh
+            ;;
+
+        2)
+            "$TOOLS_FOLDER"/modules/fixes/steam.sh
+            ;;
+
+        9)
+            post_menu
+            ;;
+
+        0)
+            exit
+            ;;
+
+        *)
+            echo -n "Unknown entry"
+            echo ""
+            fixes_menu
+            ;;
+
+        esac
+        unset input
+        fixes_menu
+}
 main_menu
