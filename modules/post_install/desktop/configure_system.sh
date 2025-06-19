637b3221b4e55bfe81a9bcbf4464adbfceb90820
Dskobra <jbttoms@gmail.com>
2025-06-19 17:55:06 -0500

    Unhide grub menu

--- modules/post_install/desktop/configure_system.sh
+++ modules/post_install/desktop/configure_system.sh
@@ -14,4 +14,7 @@ sudo systemctl enable --now cockpit.socket
 # set zram swap from default 8gb to 16gb
 sudo cp /usr/lib/systemd/zram-generator.conf /usr/lib/systemd/zram-generator.conf.bak
 sudo sed -i '/zram-size = min(ram, 8192)/c zram-size = min(ram, 16500)' /usr/lib/systemd/zram-generator.conf
+# Default Fedora hides the grub menu. I prefer it visible (like having the option when testing new kernels, nvidia driver breaks
+# or just cause)
+sudo grub2-editenv - unset menu_auto_hide
 ##########----------system----------##########
--- modules/post_install/laptop/configure_system.sh
+++ modules/post_install/laptop/configure_system.sh
@@ -12,4 +12,7 @@ sudo systemctl enable --now cockpit.socket
 
 sudo cp /usr/lib/systemd/zram-generator.conf /usr/lib/systemd/zram-generator.conf.bak
 sudo sed -i '/zram-size = min(ram, 8192)/c zram-size = min(ram, 16500)' /usr/lib/systemd/zram-generator.conf
+# Default Fedora hides the grub menu. I prefer it visible (like having the option when testing new kernels, nvidia driver breaks
+# or just cause)
+sudo grub2-editenv - unset menu_auto_hide
 ##########----------configure system----------##########
