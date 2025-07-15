a0635b2ffe4ea8d10d3c82af8599ad290f708586
Jordan Bottoms <jbttoms@gmail.com>
2025-07-15 00:07:56 -0500

    updated packages

---
+++ modules/post_install/opensuse-slowroll/LAPTOP/setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+##########----------install packages----------##########
+#"$TOOLS_FOLDER"/modules/post_install/laptop/install_packages.sh
+##########----------install packages----------##########
+
+##########----------configure hardware----------##########
+#"$TOOLS_FOLDER"/modules/post_install/laptop/configure_hardware.sh
+##########----------configure hardware----------##########
+
+##########----------configure system----------##########
+#"$TOOLS_FOLDER"/modules/post_install/laptop/configure_system.sh
+##########----------configure system----------##########
+
+##########----------configure apps----------##########
+#"$TOOLS_FOLDER"/modules/post_install/laptop/configure_apps.sh
+##########----------configure apps----------##########
+echo "Disabled atm"
