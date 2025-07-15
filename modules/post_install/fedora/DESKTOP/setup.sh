cd4633ef3f680c38f2e5df18a4c173d939719114
Jordan Bottoms <jbttoms@gmail.com>
2025-07-15 06:57:35 -0500

    updated paths

--- modules/post_install/fedora/DESKTOP/setup.sh
+++ modules/post_install/fedora/DESKTOP/setup.sh
@@ -2,17 +2,17 @@
 ## custom script to setup my desktop. Installs some additional packages
 # and does some configurations
 ##########----------install packages----------##########
-"$TOOLS_FOLDER"/modules/post_install/desktop/install_packages.sh
+"$TOOLS_FOLDER"/modules/post_install/fedora/DESKTOP/install_packages.sh
 ##########----------install packages----------##########
 
 ##########----------configure hardware----------##########
-"$TOOLS_FOLDER"/modules/post_install/desktop/configure_hardware.sh
+"$TOOLS_FOLDER"/modules/post_install/fedora/DESKTOP/configure_hardware.sh
 ##########----------configure hardware----------##########
 
 ##########----------configure system----------##########
-"$TOOLS_FOLDER"/modules/post_install/desktop/configure_system.sh
+"$TOOLS_FOLDER"/modules/post_install/fedora/DESKTOP/configure_system.sh
 ##########----------configure system----------##########
 
 ##########----------configure apps----------##########
-"$TOOLS_FOLDER"/modules/post_install/desktop/configure_apps.sh
+"$TOOLS_FOLDER"/modules/post_install/fedora/DESKTOP/configure_apps.sh
 ##########----------configure apps----------##########
