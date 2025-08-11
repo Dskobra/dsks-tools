4b67620c3ff44a06998e3e4a2c3f4271ec62ab75
Jordan Bottoms <jbttoms@gmail.com>
2025-08-11 06:54:07 -0500

    Continue splitting into functions

--- modules/post_install/fedora-atomic/DESKTOP/configure_system.sh
+++ modules/post_install/fedora-atomic/DESKTOP/configure_system.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/bash
-configure_clamd(){
-    sudo sed -i -e "/^#*LocalSocket\s/s/^#//" /etc/clamd.d/scan.conf
-    sudo freshclam
-    sudo systemctl enable --now clamav-freshclam.service clamd@scan.service
-    sudo semanage boolean -m -1 antivirus_can_scan_system
-}
-
 configure_zram(){
+    # set zram swap from default 8gb to 16gb
     cd "$TOOLS_FOLDER"/temp || exit
     touch zram-generator.conf
     echo "[zram0]" >> zram-generator.conf
@@ -21,30 +15,45 @@ build_ossec(){
     cd ossec-hids-3.8.0 || exit
     sudo ./install.sh
 }
+
+configure_security(){
+    sudo sed -i -e "/^#*LocalSocket\s/s/^#//" /etc/clamd.d/scan.conf
+    sudo freshclam
+    sudo systemctl enable --now clamav-freshclam.service clamd@scan.service
+    sudo semanage boolean -m -1 antivirus_can_scan_system
+
+    sudo sed -i '/SELINUX=enforcing/c SELINUX=permissive' /etc/selinux/config
+
+    sudo firewall-cmd --set-default-zone=home
+    sudo firewall-cmd --permanent --add-service=cockpit
+    sudo firewall-cmd --permanent --add-service=kdeconnect
+    sudo firewall-cmd --set-log-denied=all
+    sudo firewall-cmd --reload
+    sudo systemctl enable --now sshd
+}
+
+configure_system_settings(){
+    # Default Fedora hides the grub menu. I prefer it visible (like having the option when testing new kernels, nvidia driver breaks
+    # or just cause)
+
+    sudo grub2-editenv - unset menu_auto_hide
+    # https://docs.fedoraproject.org/en-US/fedora-silverblue/troubleshooting/#_unable_to_add_user_to_group
+    grep -E '^libvirt:' /usr/lib/group | sudo tee -a /etc/group
+    sudo usermod -aG libvirt $USER
+    #sudo systemctl enable --now lactd
+    #sudo modprobe ntsync
+    #sudo touch /etc/modules-load.d/ntsync.conf
+    #echo "ntsync"  | sudo tee -a /etc/modules-load.d/ntsync.conf > /dev/null
+}
 ##########----------system----------##########
-configure_clamd
-build_ossec
-sudo sed -i '/SELINUX=enforcing/c SELINUX=permissive' /etc/selinux/config
-sudo firewall-cmd --set-default-zone=home
-sudo firewall-cmd --permanent --add-service=cockpit
-sudo firewall-cmd --permanent --add-service=kdeconnect
-sudo firewall-cmd --set-log-denied=all
-sudo firewall-cmd --reload
-sudo systemctl enable --now sshd
-
-# set zram swap from default 8gb to 16gb
 configure_zram
-# Default Fedora hides the grub menu. I prefer it visible (like having the option when testing new kernels, nvidia driver breaks
-# or just cause)
+configure_security
+build_ossec
+configure_system_settings
 
-sudo grub2-editenv - unset menu_auto_hide
-# https://docs.fedoraproject.org/en-US/fedora-silverblue/troubleshooting/#_unable_to_add_user_to_group
-grep -E '^libvirt:' /usr/lib/group | sudo tee -a /etc/group
-sudo usermod -aG libvirt $USER
-#sudo systemctl enable --now lactd
-#sudo modprobe ntsync
-#sudo touch /etc/modules-load.d/ntsync.conf
-#echo "ntsync"  | sudo tee -a /etc/modules-load.d/ntsync.conf > /dev/null
+
+
+
 ##########----------system----------##########
 
 
