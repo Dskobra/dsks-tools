fa0b38765c99562c2519c5a35c8212562ccf7224
Jordan Bottoms <jbttoms@gmail.com>
2025-08-10 22:23:36 -0500

    fixed variable

--- modules/post_install/fedora-atomic/DESKTOP/install_packages.sh
+++ modules/post_install/fedora-atomic/DESKTOP/install_packages.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 ##########----------packages----------##########
-cd "TOOLS_FOLDER"/temp || exit
+cd "$TOOLS_FOLDER"/temp || exit
 sudo rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo rpm-ostree apply-live
 
