a858e6bc3e8300a2ca1408420869876d30e234e1
Jordan Bottoms <jbttoms@gmail.com>
2025-08-27 00:23:49 -0500

    added appindicator

--- modules/post_install/fedora/LAPTOP/install_packages.sh
+++ modules/post_install/fedora/LAPTOP/install_packages.sh
@@ -22,6 +22,7 @@ install_packages(){
         curl -L -o dolphin-megasync.rpm https://mega.nz/linux/repo/Fedora_42/x86_64/dolphin-megasync-5.4.0-1.1.x86_64.rpm
     elif [ "$DESKTOP_TYPE" == "GNOME" ]
     then
+        sudo dnf install -y gnome-shell-extension-appindicator
        curl -L -o nautilus-megasync.rpm https://mega.nz/linux/repo/Fedora_42/x86_64/nautilus-megasync-5.3.0-1.1.x86_64.rpm
     else
         echo "Uknown error."
