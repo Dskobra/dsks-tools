94fa6224f709639bb93ccfadef3145bd7872db8e
Jordan Bottoms <jbttoms@gmail.com>
2025-08-26 20:53:50 -0500

    added gnome packages to remove

--- modules/post_install/fedora-atomic/DESKTOP/cleanup.sh
+++ modules/post_install/fedora-atomic/DESKTOP/cleanup.sh
@@ -8,9 +8,12 @@ get_desktop_type(){
         flatpak remove -y org.kde.skanpage org.kde.okular org.kde.krdc org.kde.kolourpaint org.kde.kmines org.kde.kcalc  \
         org.kde.gwenview org.kde.elisa org.kde.kmahjongg  org.fedoraproject.Platform.tessdata  org.fedoraproject.Platform.Locale   \
         org.fedoraproject.Platform.GL.default org.fedoraproject.KDE6Platform
-    elif [ "$DESKTOP_TYPE" == "gnome " ]
+    elif [ "$DESKTOP_TYPE" == "GNOME" ]
     then
-        echo "unfinished"
+        flatpak remove -y org.gnome.font-viewer org.gnome.clocks org.gnome.baobab org.gnome.Weather org.gnome.TextEditor org.gnome.Snapshot \
+        org.gnome.NautilusPreviewer org.gnome.Maps org.gnome.Loupe org.gnome.Logs org.gnome.Extensions org.gnome.Evince org.gnome.Contacts \
+        org.gnome.Connections org.gnome.Characters org.gnome.Calendar org.gnome.Calculator org.fedoraproject.MediaWriter \
+        org.fedoraproject.Platform.Locale org.fedoraproject.Platform.GL.default org.fedoraproject.Platform
     else
         echo "Uknown error."
     fi
