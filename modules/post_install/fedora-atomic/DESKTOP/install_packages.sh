2a6811dfe3cb5816c1f53c835bd9b3fda159b6dd
Jordan Bottoms <jbttoms@gmail.com>
2025-09-05 00:42:40 -0500

    fixed wrong file

--- modules/post_install/fedora-atomic/DESKTOP/install_packages.sh
+++ modules/post_install/fedora-atomic/DESKTOP/install_packages.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-"$TOOLS_FOLDER"/modules/post_install/fedora/shared/install_packages.sh
+"$TOOLS_FOLDER"/modules/post_install/fedora-atomic/shared/install_packages.sh
 flatpak install --user -y flathub io.github.ilya_zlobintsev.LACT
 
