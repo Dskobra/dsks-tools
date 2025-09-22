9cf22ea78b4a169aab60fc8333f37638ce5bc368
Dskobra <jbttoms@protonmail.com>
2025-09-22 02:02:40 -0500

    Removed v4l2loopback driver

--- modules/post_install/fedora/shared/install_packages.sh
+++ modules/post_install/fedora/shared/install_packages.sh
@@ -6,8 +6,8 @@ install_packages(){
     sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
     sudo dnf install -y vim-enhanced toolbox distrobox openrgb i2c-tools cpu-x remmina isoimagewriter steam steam-devices gamemode.x86_64 \
     gamemode.i686 goverlay virt-manager qemu-kvm virt-install libvirt-daemon-kvm libvirt-daemon-config-network docker-compose-switch \
-    zenity wget curl flatpak dnf-plugins-core dnfdragora clamav clamav-update clamd firewall-applet akmod-v4l2loopback \
-    v4l2loopback discord cockpit cockpit-files cockpit-kdump cockpit-selinux cockpit-session-recording
+    zenity wget curl flatpak dnf-plugins-core dnfdragora clamav clamav-update clamd firewall-applet discord cockpit cockpit-files \
+    cockpit-kdump cockpit-selinux cockpit-session-recording
 
     sudo dnf group install -y c-development development-tools container-management
     sudo dnf swap ffmpeg-free ffmpeg --allowerasing
