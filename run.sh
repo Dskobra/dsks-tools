0987099c99cbd54b5401af651e32d7c995647f3c
Dskobra <jbttoms@protonmail.com>
2025-09-25 19:54:55 -0500

    Renamed vars
    Also added launcher code from setup-scripts

--- run.sh
+++ run.sh
@@ -1,11 +1,38 @@
 #!/usr/bin/bash
 
 ### Main launch script which sets some variables
+distro_check(){
+    # check if fedora
+    if [ "$DISTRO_NAME" == "fedora" ]
+    then
+        fedora_release_check
+    else
+        echo "Unfortunately, '$DISTRO_NAME $DISTRO_VER' is not a supported distro."
+    fi
 
-export TOOLS_FOLDER                             # stores full path for dsks-tools
-export DISTRO=""                                # stores distro name.
-export VERSION_ID=""
+}
+
+fedora_release_check(){
+    if [ "$DISTRO_VER" == "42" ] || [ "$DISTRO_VER" == "43" ]
+    then
+        "$TOOLS_FOLDER"/modules/core/menu.sh
+    else
+        echo "These scripts only support Fedora 42/43."
+    fi
+
+}
+
+export TOOLS_FOLDER                                                   # stores full path for dsks-tools
+export DISTRO_NAME=""                                
+export DISTRO_VER=""
+export DESKTOP_ENV=""
 export COPYRIGHT="Copyright (c) 2024-2025 Jordan Bottoms"
+
+DISTRO_NAME=$(source /etc/os-release ; echo $ID)                      # store basic distro name
+DISTRO_VER=$(source /etc/os-release ; echo "$DISTRO_VER")             # store distro version number
+DESKTOP_ENV=$(echo $XDG_CURRENT_DESKTOP)                              # store desktop name (gnome, kde etc)
+
 TOOLS_FOLDER=$(pwd)
 mkdir "$TOOLS_FOLDER"/temp
+distro_check                                                          # check if supported distro
 "$TOOLS_FOLDER"/modules/menu.sh
