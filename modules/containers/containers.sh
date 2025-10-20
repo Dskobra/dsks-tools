4856f42735b2016ad26b3d58ecf9cff699addf43
Dskobra <jbttoms@protonmail.com>
2025-10-19 19:50:20 -0500

    renamed gpu_picker
    --nvidia isnt needed for running games in distrobox and so far only
    created issues.

--- modules/containers/containers.sh
+++ modules/containers/containers.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 gpu_picker(){
+
+    # personal note: passing --nvidia isnt actually needed for running steam
+    # under distrobox. Possibly only if you are running nvidia-container-toolkit
+    # which so far only creates issues with installing libicu resulting in several
+    # programs to fail. Megasync and proton apps (iirc) are examples.
     echo "Pick a GPU."
     echo "(1) AMD                                           (2) Nvidia"
     echo "(h) Help                                          (0) Cancel"
@@ -22,8 +27,8 @@ gpu_picker(){
     fi
 }
 
-GPU_ARG=""
+#GPU_ARG=""
 CONTAINER_NAME=$1
 IMAGE="fedora:42 "
 #RELEASE="42"
-gpu_picker
+distrobox create  --name $CONTAINER_NAME --image $IMAGE
