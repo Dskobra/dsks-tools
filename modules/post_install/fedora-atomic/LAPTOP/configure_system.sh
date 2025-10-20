7310483a2746626c6198ffda1469bb82ff771005
Dskobra <jbttoms@protonmail.com>
2025-10-19 20:12:46 -0500

    added flatpak update cmd

--- modules/post_install/fedora-atomic/LAPTOP/configure_system.sh
+++ modules/post_install/fedora-atomic/LAPTOP/configure_system.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/bash
 "$TOOLS_FOLDER"/modules/post_install/fedora-atomic/shared/configure_system.sh
 cp -r "$TOOLS_FOLDER/modules/configs/game-profiles/LAPTOP" "$HOME"/.config/MangoHud/
+
+# seems flatpak only sees nvidia drivers once installed on system. So run an update during the lastp step of these
+# setup scripts
+flatpak update -y   
--- modules/post_install/fedora/LAPTOP/configure_system.sh
+++ modules/post_install/fedora/LAPTOP/configure_system.sh
@@ -3,3 +3,7 @@
 sudo sed -i '/GRUB_CMDLINE_LINUX="rhgb quiet rd.driver.blacklist=nouveau,nova_core modprobe.blacklist=nouveau,nova_core"/c GRUB_CMDLINE_LINUX="amd_iommu=on iommu=pt rd.driver.blacklist=nouveau,nova_core modprobe.blacklist=nouveau,nova_core nvidia-drm.modeset crashkernel=512M rhgb quiet"' /etc/default/grub
 sudo grub2-mkconfig -o /etc/grub2.cfg
 cp -r "$TOOLS_FOLDER/modules/configs/game-profiles/LAPTOP" "$HOME"/.config/MangoHud/
+
+# seems flatpak only sees nvidia drivers once installed on system. So run an update during the lastp step of these
+# setup scripts
+flatpak update -y   
