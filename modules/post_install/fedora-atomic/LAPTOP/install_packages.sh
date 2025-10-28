da24da202281fd5e6e154c0942081871f31d47ee
Dskobra <jbttoms@protonmail.com>
2025-10-27 23:12:55 -0500

    disabled installing open driver
    Seems like nvidia open kernel driver is installed regardless if
    akmod-nvidia-open is installed or not

--- modules/post_install/fedora-atomic/LAPTOP/install_packages.sh
+++ modules/post_install/fedora-atomic/LAPTOP/install_packages.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 attempt_to_install_broken_apps(){
     # dev tools are sometimes broken due to mismatches
-    sudo sh -c 'echo "%_with_kmod_nvidia_open 1" > /etc/rpm/macros.nvidia-kmod'
-    sudo rpm-ostree install rpmfusion-nonfree-release-tainted
-    sudo rpm-ostree apply-live
+    #sudo sh -c 'echo "%_with_kmod_nvidia_open 1" > /etc/rpm/macros.nvidia-kmod'
+    #sudo rpm-ostree install rpmfusion-nonfree-release-tainted
+    #sudo rpm-ostree apply-live
     sudo rpm-ostree install akmod-nvidia-open xorg-x11-drv-nvidia akmod-nvidia xorg-x11-drv-nvidia-cuda
-    sudo rpm-ostree kargs --append="amd_iommu=on iommu=pt rd.driver.blacklist=nouveau,nova_core modprobe.blacklist=nouveau,nova_core nvidia-drm.modeset crashkernel=512M"
+    sudo rpm-ostree kargs --append="amd_iommu=on iommu=pt rd.driver.blacklist=nouveau,nova_core modprobe.blacklist=nouveau,nova_core crashkernel=512M"
 
 }
 
