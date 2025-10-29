8f4d156ac99206f4016d0631e2e1ebd122c6c46c
Dskobra <jbttoms@protonmail.com>
2025-10-28 23:48:16 -0500

    moved installing mesa codecs to shared script

--- modules/post_install/fedora/DESKTOP/install_packages.sh
+++ modules/post_install/fedora/DESKTOP/install_packages.sh
@@ -3,10 +3,4 @@
 sudo dnf copr enable -y ilyaz/LACT
 sudo dnf install -y lact
 
-sudo dnf swap -y mesa-va-drivers mesa-va-drivers-freeworld
-sudo dnf swap -y mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686
-
-sudo dnf swap -y mesa-vulkan-drivers mesa-vulkan-drivers-freeworld
-sudo dnf swap -y mesa-vulkan-drivers.i686 mesa-vulkan-drivers-freeworld.i686
-
 zenity --warning --text="Reminder to restart terminal so it sees nodejs to install bash lsp"
--- modules/post_install/fedora/shared/install_packages.sh
+++ modules/post_install/fedora/shared/install_packages.sh
@@ -2,15 +2,31 @@
 
 ### shared packages between my devices.
 install_fedora_rpmfusion_packages(){
-    
     sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-    sudo dnf install -y vim-enhanced toolbox distrobox openrgb i2c-tools cpu-x remmina isoimagewriter steam steam-devices gamemode.x86_64 \
+    sudo dnf install -y vim-enhanced toolbox distrobox openrgb cpu-x remmina isoimagewriter steam steam-devices gamemode.x86_64 \
     gamemode.i686 goverlay virt-manager qemu-kvm virt-install libvirt-daemon-kvm libvirt-daemon-config-network docker-compose-switch \
-    zenity wget curl flatpak dnf-plugins-core clamav clamav-update clamd firewall-applet discord cockpit cockpit-files cockpit-kdump \
-    cockpit-selinux cockpit-session-recording ptyxis
+    wget curl flatpak dnf-plugins-core clamav clamav-update clamd firewall-applet discord cockpit cockpit-files cockpit-kdump \
+    cockpit-selinux cockpit-session-recording 
 
-    sudo dnf group install -y c-development development-tools container-management
+    sudo dnf group install -y c-development development-tools
     sudo dnf -y swap ffmpeg-free ffmpeg --allowerasing
+
+    sudo dnf swap -y mesa-va-drivers mesa-va-drivers-freeworld
+    sudo dnf swap -y mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686
+
+    sudo dnf swap -y mesa-vulkan-drivers mesa-vulkan-drivers-freeworld
+    sudo dnf swap -y mesa-vulkan-drivers.i686 mesa-vulkan-drivers-freeworld.i686
+    if [ "$XDG_CURRENT_DESKTOP" == "KDE" ]
+    then
+        sudo rpm-ostree install zenity ptyxis
+    elif [ "$XDG_CURRENT_DESKTOP" == "GNOME" ]
+    then
+        sudo dnf install -y i2c-tools kde-partitionmanager
+    else
+        echo "$XDG_CURRENT_DESKTOP is not supported."
+    fi
+
+
 }
 
 install_other_apps(){
@@ -36,12 +52,17 @@ install_other_apps(){
         echo "$XDG_CURRENT_DESKTOP is not supported."
     fi
 
-    curl -L -o proton-mail.rpm https://proton.me/download/mail/linux/1.9.0/ProtonMail-desktop-beta.rpm
+    curl -L -o proton-mail.rpm https://proton.me/download/mail/linux/1.9.1/ProtonMail-desktop-beta.rpm
     curl -L -o proton-pass.rpm https://proton.me/download/pass/linux/proton-pass-1.32.3-1.x86_64.rpm
     curl -L -o proton-authenticator.rpm https://proton.me/download/authenticator/linux/ProtonAuthenticator-1.0.0-1.x86_64.rpm
 
     sudo dnf install -y *.rpm
     rm *.rpm
+
+    # install nodejs
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+    source ~/.bashrc
+    nvm install lts/*
 }
 install_flatpaks(){
     flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
