2269322ae2df992c0d90fdfd08c8b9bbf912355d
Dskobra <jbttoms@protonmail.com>
2025-10-29 22:12:43 -0500

    moving fedora non-atomic code here

--- modules/post_install/fedora/DESKTOP/install_packages.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-"$TOOLS_FOLDER"/modules/post_install/fedora/shared/install_packages.sh
-#sudo dnf copr enable -y ilyaz/LACT
-#sudo dnf install -y lact
-
-#zenity --warning --text="Reminder to restart terminal so it sees nodejs to install bash lsp"
