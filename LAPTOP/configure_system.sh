342f5f1b640b8a227a54b73824587e49fe7fad85
Dskobra <jbttoms@protonmail.com>
2025-10-31 00:30:12 -0500

    update pathing
    Signed-off-by: Dskobra <jbttoms@protonmail.com>

--- LAPTOP/configure_system.sh
+++ LAPTOP/configure_system.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 "$TOOLS_FOLDER"/modules/post-install/distro/shared/configure_system.sh
-cp -r "$TOOLS_FOLDER/modules/configs/game-profiles/LAPTOP" "$HOME"/.config/MangoHud/
+cp -r "$TOOLS_FOLDER/modules/game-profiles/LAPTOP" "$HOME"/.config/MangoHud/
 
 # seems flatpak only sees nvidia drivers once installed on system. So run an update during the last step of these
 # setup scripts
