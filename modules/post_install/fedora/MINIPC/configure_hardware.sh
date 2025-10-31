eb4eb122b57cee35e671fa3fd51acda2dfde9c7b
Dskobra <jbttoms@protonmail.com>
2025-10-31 00:18:15 -0500

    use new layout
    Signed-off-by: Dskobra <jbttoms@protonmail.com>

--- modules/post_install/fedora/MINIPC/configure_hardware.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-configure_boot(){
-    sudo sed -i '/GRUB_CMDLINE_LINUX="rhgb quiet"/c GRUB_CMDLINE_LINUX="amd_iommu=on iommu=pt crashkernel=512M rhgb quiet"' /etc/default/grub
-    sudo sed -i '/GRUB_TIMEOUT=5/c GRUB_TIMEOUT=12' /etc/default/grub
-    sudo grub2-mkconfig -o /etc/grub2.cfg
-}
-
-configure_boot
