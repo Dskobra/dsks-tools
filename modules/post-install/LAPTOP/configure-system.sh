db4c55b14faa1c7bb7002e5d4cc9f11ed219c467
Dskobra <jbttoms@protonmail.com>
2025-11-11 04:39:18 -0600

    fixed paths for grub configs
    Signed-off-by: Dskobra <jbttoms@protonmail.com>

--- modules/post-install/LAPTOP/configure-system.sh
+++ modules/post-install/LAPTOP/configure-system.sh
@@ -4,7 +4,7 @@ configure_grub(){
     #sudo sed -i '/GRUB_CMDLINE_LINUX="rhgb quiet rd.driver.blacklist=nouveau,nova_core modprobe.blacklist=nouveau,nova_core"/c GRUB_CMDLINE_LINUX="amd_iommu=on iommu=pt rd.driver.blacklist=nouveau,nova_core modprobe.blacklist=nouveau,nova_core crashkernel=512M rhgb quiet"' /etc/default/grub
     #sudo grubby --args="amd_iommu=on iommu=pt rd.driver.blacklist=nouveau,nova_core modprobe.blacklist=nouveau,nova_core crashkernel=512M rhgb quiet" --update-kernel=ALL
     sudo cp /etc/default/grub /etc/default/grub-og.bak
-    sudo cp "$TOOLS_FOLDER"/modules/post-install/fedora/LAPTOP/grub-fedora-laptop /etc/default/grub
+    sudo cp "$TOOLS_FOLDER"/modules/post-install/LAPTOP/grub-fedora-laptop /etc/default/grub
     sudo chown root:root /etc/default/grub
     sudo grub2-mkconfig -o /etc/grub2.cfg
 }
