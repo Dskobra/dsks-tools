d7da223bfc244fb9e9fac8ca80e5aec84c479b6c
Dskobra <jbttoms@protonmail.com>
2025-11-14 00:20:32 -0600

    cleanup
    Signed-off-by: Dskobra <jbttoms@protonmail.com>

--- run.sh
+++ run.sh
@@ -3,10 +3,6 @@
 ### Main launch script which sets some variables
 
 export TOOLS_FOLDER
-export DISTRO_FOLDER                                                   
-export DISTRO_NAME=""                                
-export DISTRO_VER=""
-export OSTREE_VER=""
 export COPYRIGHT="Copyright (c) 2024-2025 Jordan Bottoms"
 
 DISTRO_NAME=$(source /etc/os-release ; echo $ID)                      # store basic distro name
@@ -14,6 +10,5 @@ DISTRO_VER=$(source /etc/os-release ; echo "$VERSION_ID")             # store di
 OSTREE_VER=$(source /etc/os-release ; echo "$OSTREE_VERSION")
 
 TOOLS_FOLDER=$(pwd)                                                   # stores full path for dsks-tools
-DISTRO_FOLDER="$TOOLS_FOLDER"/modules/post-install/distro
 mkdir "$TOOLS_FOLDER"/temp                                            # check if supported distro
 "$TOOLS_FOLDER"/modules/core/menu.sh
