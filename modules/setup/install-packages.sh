c4d621be7a1ce0864e8fa290fd6241c74989c5da
Dskobra <jbttoms@protonmail.com>
2025-11-14 08:21:28 -0600

    cleanup
    Signed-off-by: Dskobra <jbttoms@protonmail.com>

--- modules/setup/install-packages.sh
+++ modules/setup/install-packages.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
-################################
-### section for fedora /w dnf
-################################
 install_fedora_packages(){
     sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
     sudo dnf install -y rpmfusion-free-release-tainted
+    sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+    wget https://mega.nz/linux/repo/Fedora_43/x86_64/megasync-Fedora_43.x86_64.rpm && sudo dnf install -y "$PWD/megasync-Fedora_43.x86_64.rpm"
     sudo dnf install -y vim-enhanced toolbox distrobox openrgb cpu-x remmina isoimagewriter steam steam-devices gamemode.x86_64 \
     gamemode.i686 goverlay virt-manager qemu-kvm virt-install libvirt-daemon-kvm libvirt-daemon-config-network docker-compose-switch \
     wget curl flatpak dnf-plugins-core clamav clamav-update clamd firewall-applet discord cockpit cockpit-files cockpit-kdump \
-    cockpit-selinux cockpit-session-recording vlc openshot tilix
+    cockpit-selinux cockpit-session-recording vlc openshot tilix brave-browser
+    
 
     # codecs some stuff is taken from the below guide
     # https://github.com/devangshekhawat/Fedora-43-Post-Install-Guide
@@ -25,47 +25,28 @@ install_fedora_packages(){
     sudo dnf install -y openh264 gstreamer1-plugin-openh264 mozilla-openh264 libdvdcss
     sudo dnf upgrade -y @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 
+    cd "$TOOLS_FOLDER/temp" || exit
     if [ "$XDG_CURRENT_DESKTOP" == "KDE" ]
     then
+        curl -L -o dolphin-megasync.rpm https://mega.nz/linux/repo/Fedora_43/x86_64/dolphin-megasync-5.4.0-2.1.x86_64.rpm
         sudo dnf install -y k3b
     elif [ "$XDG_CURRENT_DESKTOP" == "GNOME" ]
     then
+        curl -L -o nautilus-megasync.rpm https://mega.nz/linux/repo/Fedora_43/x86_64/nautilus-megasync-5.4.0-1.1.x86_64.rpm
         sudo dnf install -y gnome-shell-extension-appindicator gnome-tweaks dconf-editor file-roller xfburn \
         i2c-tools kde-partitionmanager
     else
         echo "$XDG_CURRENT_DESKTOP is not supported."
     fi
 
+    curl -L -o proton-mail.rpm $PROTON_MAIL
+    curl -L -o proton-pass.rpm $PROTON_PASS
+    curl -L -o proton-authenticator.rpm $PROTON_AUTH
+    sudo dnf install -y *.rpm
+    rm *.rpm
+    sudo dnf remove -y libreoffice*
+
 }
-
-install_fedora_third_party_packages(){
-    cd "$TOOLS_FOLDER/temp" || exit
-    sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-    sudo dnf install -y brave-browser
-    wget https://mega.nz/linux/repo/Fedora_43/x86_64/megasync-Fedora_43.x86_64.rpm && sudo dnf install -y "$PWD/megasync-Fedora_43.x86_64.rpm"
-    wget "https://repo.protonvpn.com/fedora-$(cat /etc/fedora-release | cut -d' ' -f 3)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.3-1.noarch.rpm"
-    sudo dnf install -y ./protonvpn-stable-release-1.0.3-1.noarch.rpm && sudo dnf check-update -y --refresh
-    sudo dnf install -y  proton-vpn-gnome-desktop
-    rm protonvpn-stable-release-1.0.3-1.noarch.rpm
-
-    if [ "$XDG_CURRENT_DESKTOP" == "KDE" ]
-    then
-        curl -L -o dolphin-megasync.rpm https://mega.nz/linux/repo/Fedora_43/x86_64/dolphin-megasync-5.4.0-2.1.x86_64.rpm
-    elif [ "$XDG_CURRENT_DESKTOP" == "GNOME" ]
-    then
-        curl -L -o nautilus-megasync.rpm https://mega.nz/linux/repo/Fedora_43/x86_64/nautilus-megasync-5.4.0-1.1.x86_64.rpm
-    else
-        echo "$XDG_CURRENT_DESKTOP is not supported."
-    fi
-}
-
-################################
-### end section
-################################
-
-################################
-### section for openSUSE
-################################
 install_opensuse_packages(){
     cd "$TOOLS_FOLDER/temp" || exit
     sudo zypper -n install --auto-agree-with-licenses setroubleshoot-server git git-gui gh git-cola python313-idle \
@@ -87,9 +68,16 @@ install_opensuse_packages(){
     else
         echo "$XDG_CURRENT_DESKTOP is not supported."
     fi
-}
+    cd "$TOOLS_FOLDER/temp" || exit
+    curl -L -o proton-mail.rpm $PROTON_MAIL
+    curl -L -o proton-pass.rpm $PROTON_PASS
+    curl -L -o proton-authenticator.rpm $PROTON_AUTH
+    sudo zypper -n install *.rpm
+    rm *.rpm
+    
+    sudo zypper -n dup
+    sudo opi -n codecs
 
-cleanup_opensuse_packages(){
     sudo zypper -n rm lftp kmahjongg kmines kreversi ksudoku icewm icewm-config-upstream \
     yast2-firewall yast2-network yast2-country yast2-printer yast2-proxy yast2-scanner \
     yast2-services-manager xscreensaver xscreensaver-data gnome-mahjongg swell-foop \
@@ -97,12 +85,7 @@ cleanup_opensuse_packages(){
     totem gnome-photos xterm yast2-kdump
 }
 
-################################
-### end section
-################################
-
-
-install_packages(){
+install_other(){
     # install nodejs
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
     source ~/.bashrc
@@ -126,11 +109,6 @@ install_packages(){
     flatpak install --user -y flathub org.raspberrypi.rpi-imager com.obsproject.Studio io.podman_desktop.PodmanDesktop \
     org.qownnotes.QOwnNotes
 
-    cd "$TOOLS_FOLDER/temp" || exit
-    curl -L -o proton-mail.rpm https://proton.me/download/mail/linux/1.10.1/ProtonMail-desktop-beta.rpm
-    curl -L -o proton-pass.rpm https://proton.me/download/pass/linux/proton-pass-1.32.11-1.x86_64.rpm
-    curl -L -o proton-authenticator.rpm https://proton.me/download/authenticator/linux/ProtonAuthenticator-1.1.4-1.x86_64.rpm
-
     if [ "$XDG_CURRENT_DESKTOP" == "KDE" ]
     then
         echo ""
@@ -142,25 +120,20 @@ install_packages(){
     fi
 }
 
+PROTON_MAIL="https://proton.me/download/mail/linux/1.10.1/ProtonMail-desktop-beta.rpm"
+PROTON_PASS="https://proton.me/download/pass/linux/proton-pass-1.32.11-1.x86_64.rpm"
+PROTON_AUTH="https://proton.me/download/authenticator/linux/ProtonAuthenticator-1.1.4-1.x86_64.rpm"
 echo "Desktop is $XDG_CURRENT_DESKTOP"
+
 if [ "$1" == "fedora" ]
 then
     install_fedora_packages
-    install_fedora_third_party_packages
-    cd "$TOOLS_FOLDER/temp" || exit
-    sudo dnf install -y *.rpm
-    rm *.rpm
-    sudo dnf remove -y libreoffice*
+    install_other
 elif [ "$1" == "opensuse" ]
 then
     install_opensuse_packages
-    cleanup_opensuse_packages
-    cd "$TOOLS_FOLDER/temp" || exit
-    sudo zypper -n --no-gpg-checks install  *.rpm
-    rm *.rpm
+    install_other
 else
     echo "error"
 fi
-
-install_packages
 zenity --warning --text="Reminder to restart terminal so it sees nodejs to install bash lsp"
